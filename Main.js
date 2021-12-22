0d839b8bbeba81b00437a31fd1ad6ab7134e02e1
Ganeah <83331986+Ganeah@users.noreply.github.com>
2021-12-22 18:40:29 +0530

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,14 @@
+function preload(){
+    
+}
+
+function setup(){
+    createCanvas(400,400);    
+}
+
+function draw(){   
+    rect(250,40,50);
+    circle(350,60,70);    
+    fill(350,128,0);
+    stroke(350,128,0);
+}
